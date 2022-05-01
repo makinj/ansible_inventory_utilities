ac910f64e16c2850bdc9e9b62c75497ad22c7ae1
Joshua Makinen <makinj64@gmail.com>
2022-04-30 23:35:57 -0700

    Create inverted-group.py

---
+++ inverted-group.py
@@ -0,0 +1,75 @@
+#!/bin/env python
+
+
+  
+DOCUMENTATION = '''
+    name: inverted-group Inventory
+    plugin_type: inventory
+    author:
+      - Joshua Makinen (@joshuamakinen)
+    short_description: TKTK.
+    description:
+        - TKTK
+    version_added: "n/a"
+    inventory: inverted-group
+    options:
+        plugin:
+            description: Token that ensures this is a source file for the plugin.
+            required: True
+            choices: ['inverted-group']
+        hosts:
+            description:
+                - TKTK
+            required: False
+        groups:
+            description:
+                - TKTK
+            required: False
+    requirements:
+        - python >= 2.7
+'''
+EXAMPLES = r'''
+# example inverted-group.yml file
+---
+plugin: inverted-group
+hosts:
+    example-host:
+        parents:
+            dev_tmux_installed
+groups:
+    dev_tmux_installed:
+        parents:
+            dev
+            tmux_installed
+'''
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.inventory import BaseFileInventoryPlugin
+
+class InventoryModule(BaseFileInventoryPlugin):
+
+    NAME = 'inverted-group'
+
+    def verify_file(self, path):
+      super(InventoryModule, self).verify_file(path)
+      return path.endswith(('igroup.yml', 'igroup.yaml','inverted-group.yml', 'inverted-group.yaml'))
+
+    def parse(self, inventory, loader, path, cache=True):
+        super(InventoryModule, self).parse(inventory, loader, path)
+        self._read_config_data(path)
+
+        hosts_in = self.get_option('hosts')
+        if hosts_in:
+            for host, hostdata in hosts_in.items():
+                for parent in hostdata['parents']:
+                    self.inventory.add_group(parent)
+                    self.inventory.add_host(host,parent)
+
+        groups_in = self.get_option('groups')
+        if groups_in:
+            for group, groupdata in groups_in.items():
+                self.inventory.add_group(group)
+                for parent in groupdata['parents']:
+                    self.inventory.add_group(parent)
+                    self.inventory.add_child(parent,group)
+
